c483ebaf2cfbc6cb857f36e232c3d8551a72fac9
Jarad DeLorenzo <jaradd@gmail.com>
2013-08-11 12:00:30 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SagepointPortal::Application.config.secret_key_base = '89cc2fb73f59d0a6759c749500335a267fedad5947cd3fea5e33c2bfae211c42753b7cdddf8afeac642e741aeb86010eb831fb479bc36eb41f3adb70d236ee39'
