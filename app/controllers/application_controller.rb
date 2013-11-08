e1cb2c8259c168afaee111718658654fd8c0d17e
Jarad DeLorenzo <jaradd@gmail.com>
2013-11-07 21:25:08 -0500

    Fix for cross-origin access problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :reset_session
     respond_to :json
 
-	after_filter :set_access_control_headers
+	before_filter :set_access_control_headers
 
 	def set_access_control_headers
-		headers['Access-Control-Allow-Origin'] = '*'
-		headers['Access-Control-Request-Method'] = '*'
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
+		response.headers['Access-Control-Request-Method'] = 'GET, PUT, POST, DELETE, OPTIONS'
 	end
 end
