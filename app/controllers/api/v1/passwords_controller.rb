5b95c329542013bd03cf434acd4bf22de9ec4b57
Jarad DeLorenzo <jdelorenzo@sagepointsoftware.com>
2013-11-08 23:12:54 +0000

    wtfff cors

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,29 +1,31 @@
 class Api::V1::PasswordsController < Devise::PasswordsController
 
-  prepend_before_filter :require_no_authentication, :only => [:create]
-  skip_before_filter :verify_authenticity_token #, :if => Proc.new { |c| c.request.format == 'application/json' }
-  before_filter :validate_auth_token, :except => :create
+#  prepend_before_filter :require_no_authentication, :only => [:create]
+#  skip_before_filter :verify_authenticity_token #, :if => Proc.new { |c| c.request.format == 'application/json' }
+#  before_filter :validate_auth_token, :except => :create
   include Devise::Controllers::Helpers
   include ApiHelper
   respond_to :json
 
   #TODO
   #REFACTOR: extract method
-  before_filter :set_access_control_headers
+  after_filter :set_access_control_headers
 
   def set_access_control_headers
+	Rails.logger.info("SETTING HEADERS")
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Allow-Headers'] = 'origin, x-requested-with, content-type, accept, authorization'
   end
   #REFACTOR: end
 
   def create
-  	resource = User.find_for_database_authentication(:email => params[:email])
+	Rails.logger.info("hihihi")
+  	resource = User.find_by_email(params[:email])
   	return failure unless resource
 
-  	resource.send_reset_password_instructions
+#  	resource.send_reset_password_instructions
   	render :json=> {:success => true, :message => "Reset instructions sent to #{resource.email}"}
 
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,5 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :reset_session
     respond_to :json
 
-  #TODO
-  #REFACTOR: extract method
-  before_filter :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
-  #REFACTOR: end
 
 end
