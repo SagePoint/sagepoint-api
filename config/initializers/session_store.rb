03db65907cff35ae9521c8c61c3a6e2edff60383
Jarad DeLorenzo <jaradd@gmail.com>
2013-11-24 21:14:03 -0330

    Removed redundant memcached config setting - possible was overriding environment-specific setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-SagepointApi::Application.config.session_store :mem_cache_store, {:namespace => "SAGEPOINT-API"}
+#SagepointApi::Application.config.session_store :mem_cache_store, {:namespace => "SAGEPOINT-API"}
 #SagepointApi::Application.config.session_store :active_record_store
